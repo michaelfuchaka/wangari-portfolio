fcc064f1b69646d13efebe58096f7b8321342b01
michaelfuchaka <fuchakamichael06@gmail.com>
2026-04-10 22:35:32 +0300

    Update next-env and next.config files for improved type references and configuration settings

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {}
-
-export default nextConfig 
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+export default nextConfig;
